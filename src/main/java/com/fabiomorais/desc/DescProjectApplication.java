dfec0cbc9be8a23d71ef31d80e9d13ecdb30972c
Fabio Morais <fabio.lmorais99@gmail.com>
2024-06-29 12:21:50 -0300

    first commit

---
+++ src/main/java/com/fabiomorais/desc/DescProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fabiomorais.desc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescProjectApplication.class, args);
+	}
+
+}
