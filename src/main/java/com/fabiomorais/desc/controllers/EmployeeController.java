d1695545589283018e004222d3820fbdcd0baaf9
Fabio Morais <fabio.lmorais99@gmail.com>
2024-07-02 20:08:11 -0300

    Feat: Criado o metodo findAll paginado

--- src/main/java/com/fabiomorais/desc/controllers/EmployeeController.java
+++ src/main/java/com/fabiomorais/desc/controllers/EmployeeController.java
@@ -3,6 +3,8 @@ package com.fabiomorais.desc.controllers;
 import com.fabiomorais.desc.dto.EmployeeDTO;
 import com.fabiomorais.desc.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,12 @@ public class EmployeeController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<EmployeeDTO> findById(@PathVariable Long id) {
         EmployeeDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EmployeeDTO>> findAll(Pageable pageable) {
+        Page<EmployeeDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/com/fabiomorais/desc/services/EmployeeService.java
+++ src/main/java/com/fabiomorais/desc/services/EmployeeService.java
@@ -4,7 +4,10 @@ import com.fabiomorais.desc.dto.EmployeeDTO;
 import com.fabiomorais.desc.entities.Employee;
 import com.fabiomorais.desc.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmployeeService {
@@ -12,8 +15,15 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository repository;
 
+    @Transactional(readOnly = true)
     public EmployeeDTO findById(Long id) {
         Employee employee = repository.findById(id).get();
         return new EmployeeDTO(employee);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<EmployeeDTO> findAll(Pageable pageable) {
+        Page<Employee> result = repository.findAll(pageable);
+        return result.map(x -> new EmployeeDTO(x));
     }
 }
